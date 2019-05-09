27157d8b74b0b5fe77db3d4ae98c959e1c74b9ac
Filippo Benozzi <filippo.benozzi@starsoftware.it>
2019-05-09 15:21:29 +0200

    implementazione dei gruppi

--- wp-content/themes/default-theme/template/tmpl-functions.php
+++ wp-content/themes/default-theme/template/tmpl-functions.php
@@ -30,5 +30,13 @@
         include get_template_directory() . '/template/components/' . $component_name . '.php';
     }
     function render_theme_block($block_name,$args = []){
+
+        if( is_string($args['id_block']) ){
+            $fields = get_field($args['id_block']);
+        } else {
+            $fields = $args['id_block'];
+        }
+
         include get_template_directory() . '/template/blocks/' . $block_name . '.php';
+
     }
