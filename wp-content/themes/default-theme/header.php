abc8def9f9556859d79045564f52be6daf38b7af
Filippo Benozzi <filippo.benozzi@starsoftware.it>
2019-05-13 15:00:48 +0200

    add title

--- wp-content/themes/default-theme/header.php
+++ wp-content/themes/default-theme/header.php
@@ -13,6 +13,7 @@
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1">
 
     <?php wp_head(); ?>
+    <title><?php wp_title('&ndash;','true','right'); ?><?php bloginfo('name'); ?></title>
     <meta name="keywords" content="#" />
 
     <meta name="author" content="Dinamiza">
