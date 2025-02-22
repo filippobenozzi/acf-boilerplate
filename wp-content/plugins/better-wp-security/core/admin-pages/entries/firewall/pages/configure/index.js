79e7b9a0d29079bf5679c526bf8fdcacc8233bea
Filippo Benozzi <filippobenozzi@MBP-di-Filippo.local>
2025-02-22 17:41:36 +0100

    update all to run with bun webpack and node 18

--- wp-content/plugins/better-wp-security/core/admin-pages/entries/firewall/pages/configure/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * Internal dependencies
- */
-import { Page } from '../../components';
-
-export default function Configure() {
-	return <Page>Configure</Page>;
-}
--- wp-content/plugins/better-wp-security/core/admin-pages/entries/firewall/pages/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Logs } from './logs';
-export { default as Rules } from './rules';
-export { default as Rule } from './rule';
-export { default as Configure } from './configure';
